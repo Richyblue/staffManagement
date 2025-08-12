a8f90d785791be6b77325a123f4d9e5a27962ea9
Jimmy Richard <60008417+Richyblue@users.noreply.github.com>
2025-08-12 09:14:45 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,8 @@ import axios from "axios";
 
 function Home() {
     const BASE_URL = process.env.REACT_APP_BASE_URL;
-    const [totalStaff, setTotalStaff] = useState(null);
+const [totalStaff, setTotalStaff] = useState(null);
+    const [totalSuspended, setTotalSuspended] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
@@ -31,6 +32,26 @@ function Home() {
         setLoading(false);
       });
   }, []);
+
+    useEffect(() => {
+    const token = localStorage.getItem("token"); // get token from localStorage
+
+    axios
+      .get(`${BASE_URL}api/v1/staffsuspendedcount`, {
+        headers: {
+          Authorization: `Bearer ${token}`, // attach token
+        },
+      })
+      .then((res) => {
+        setTotalSuspended(res.data.suspendedCount); // matches { total: totalCount }
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.response?.data?.message || err.message);
+        setLoading(false);
+      });
+  }, []);
+
 
         return (
             <div>
@@ -63,7 +84,7 @@ function Home() {
                         <button
                             className="mt-4 w-full bg-yellow-200 text-gray-700 shadow-lg p-2 rounded hover:bg-gray-400 transition"
                         >
-                            <h2 className="font-bold sm:text-lg p-3">0</h2>
+                            <h2 className="font-bold sm:text-lg p-3">{totalSuspended}</h2>
                             <FontAwesomeIcon icon={faRefresh}/>  Suspended Staff
                         </button>
                         <Link to='/leave'>
